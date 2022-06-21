c90f9dd0f3911238f7c47a493a93a6879cf45117
Hao Yu Wang <awaitcabal@hotmail.com>
2022-06-22 02:03:39 +0800

    [update] job opening page layout html css

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, "public")));
-app.set("view engine", "pug");
 
 // Route
 
