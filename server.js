baab2c9909cb29306a2ae7fd5000d6f4cd371e04
Hao Yu Wang <awaitcabal@hotmail.com>
2022-06-21 08:53:58 +0800

    [update] view opening all template

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express');
+const path = require("path");
+
+const { PORT } = process.env
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "public")));
+app.set("view engine", "pug");
+
+// Route
+
+
+
+app.use('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"))
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server listen on port: ${PORT}`)
+})
