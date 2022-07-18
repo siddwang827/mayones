20deb76b6cc7b6085088de9e48fbc5aeb837066a
Hao Yu Wang <awaitcabal@hotmail.com>
2022-07-18 08:07:29 +0800

    [fix] parameter bug

--- server/models/mysql_conn.js
+++ server/models/mysql_conn.js
@@ -4,23 +4,14 @@ const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_CONNECTION_LIMIT } = 
 
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Sac10421',
-    database: 'mayones',
+    host: DB_HOST,
+    user: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    connectionLimit: DB_CONNECTION_LIMIT,
+    waitForConnections: true,
+    queueLimit: 0
 });
-
-
-
-// const pool = mysql.createPool({
-//     host: DB_HOST,
-//     user: DB_USERNAME,
-//     password: DB_PASSWORD,
-//     database: DB_DATABASE,
-//     connectionLimit: DB_CONNECTION_LIMIT,
-//     waitForConnections: true,
-//     queueLimit: 0
-// });
 
 
 async function queryDB(sql, params) {
